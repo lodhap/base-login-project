48a522f07051d5087b4bb6f233e2113afbb9cc9f
lodhap <7834jsb@gmail.com>
2024-04-02 00:07:51 +0900

    init

---
+++ base-login-project/src/main/java/com/example/BaseLoginProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseLoginProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseLoginProjectApplication.class, args);
+	}
+
+}
